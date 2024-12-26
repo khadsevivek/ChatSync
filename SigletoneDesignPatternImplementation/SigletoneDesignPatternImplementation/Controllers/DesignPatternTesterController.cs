a0781227b8e4e04e2d27c612cdd1c30dfb69d88a
khadsevivek39 <khadsevivek39@gmail.com>
2024-12-26 16:53:44 +0530

    Simple Implementation of Sigletone Design Pattern in .Net

---
+++ SigletoneDesignPatternImplementation/SigletoneDesignPatternImplementation/Controllers/DesignPatternTesterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SigletoneDesignPatternImplementation.Services;
+
+namespace SigletoneDesignPatternImplementation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesignPatternTesterController : ControllerBase
+    {
+        [HttpGet("First")]
+        public async Task<ActionResult> TestFirstInstence()
+        {
+            var obj = Singletone.getInstence();
+            return Ok(Singletone.TestId);
+        }
+        [HttpGet("Second")]
+        public async Task<ActionResult> TestSecoundInstence()
+        {
+            var obj = Singletone.getInstence();
+            return Ok(Singletone.TestId);
+        }
+    }
+}
---
+++ SigletoneDesignPatternImplementation/SigletoneDesignPatternImplementation/Services/Singletone.cs
@@ -0,0 +1,32 @@
+﻿namespace SigletoneDesignPatternImplementation.Services
+{
+    public class Singletone
+    {
+        //Atchual instence
+        private static Singletone _instence { get; set; }
+        //Lock object verible for thread safety
+        private static readonly object _lock  = new();
+        //Constructor to prevent object creation
+        public static Guid TestId { get; private set; }
+        private Singletone(){}
+
+        //method to return the instence of a singletone class
+        public static Singletone getInstence()
+        {
+            if(_instence == null)
+            {
+                //Lock new thred till completion of old one
+                lock (_lock)
+                {
+                    if (_instence == null)
+                    {
+                        _instence = new Singletone();
+                        TestId = Guid.NewGuid();
+                    }
+                }
+                
+            }
+            return _instence;
+        }
+    }
+}
